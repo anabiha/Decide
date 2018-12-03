fb69e7a58b240eb6b26ede8b54516a4d47f36cc6
Daniel <danielwei816@gmail.com>
2018-12-02 16:26:11 -0800

    sup

--- Decide/NewDecisionViewController.swift
+++ Decide/NewDecisionViewController.swift
@@ -7,32 +7,80 @@
 //
 
 import UIKit
-class NewDecisionViewController: UIViewController, UITableViewDelegate {
+class NewDecisionViewController: UIViewController, UITableViewDelegate,  UITableViewDataSource {
     
     @IBOutlet weak var decisionTitle: UITextField!
     @IBOutlet weak var tableView: UITableView!
     
     var decision = Decision()
+    var decisionItemCount = 2;
+    let cellReuseIdentifier = "decisionItemCell"
+    let cellSpacingHeight: CGFloat = 15
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.register(DecisionItem.self, forCellReuseIdentifier: cellReuseIdentifier)
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.tableFooterView = UIView()
     }
     //some function that, once tapped, saves the title and all decisions
     
-    //function to hide the cells until add decisionitem button is tapped
+   
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return decisionItemCount
+    }
     
-//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-//        // 1
-//        guard let identifier = segue.identifier else { return }
-//
-//        // 2
-//        switch identifier {
-//        case "cancel":
-//            print("cancel button tapped")
-//        case "save":
-//            print("save button tapped")
-//        default:
-//            print("unexpected segue identifier")
-//        }
-//    }
+    // There is just one row in every section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    // Set the spacing between sections
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return cellSpacingHeight
+    }
+    
+    // Make the background color show through
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = UIColor.clear
+        return headerView
+    }
+    // create a cell for each table view row
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        if indexPath.section == decisionItemCount - 1 { //if the selected row is the add row, note that indexPath.section is used rather than indexPath.row
+            let cell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! DecisionItem
+        
+        // add border and color
+        let grayColor = UIColor(red: 235, green: 235, blue: 235, alpha: 1)
+        cell.backgroundColor = grayColor
+        cell.layer.borderColor = grayColor.cgColor
+        cell.layer.borderWidth = 1
+        cell.layer.cornerRadius = 8
+        cell.clipsToBounds = true
+        
+        return cell
+        } else { //if it's not the add row....
+            let cell = self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as! DecisionItem //cast to decisionitem
+            cell.backgroundColor = UIColor.white
+            cell.layer.borderColor = UIColor.black.cgColor
+            cell.layer.borderWidth = 1
+            cell.layer.cornerRadius = 8
+            cell.clipsToBounds = true
+            return cell
+        }
+    }
+    
+    // method to run when table view cell is tapped
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        // note that indexPath.section is used rather than indexPath.row
+        print("You tapped cell number \(indexPath.section).")
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        //the height of the post, to be implemented later
+        return 50
+    }
 }
